2b7402629e598e0246505801c2f9ddd1850594a2
xmba15 <thba1590@gmail.com>
2019-07-20 12:07:21 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Spring-Boot Based Web Application to Store Web URL for Browsing Later #
---
+++ src/main/java/com/bookmarkapp/bookmark_url/BookmarkUrlApplication.java
@@ -0,0 +1,13 @@
+package com.bookmarkapp.bookmark_url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkUrlApplication.class, args);
+	}
+
+}
