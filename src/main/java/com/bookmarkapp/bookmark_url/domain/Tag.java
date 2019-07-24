ed020e5f37f57a8a77b3b1241d9c62b446715585
xmba15 <thba1590@gmail.com>
2019-07-24 09:24:40 +0900

    add domain

---
+++ src/main/java/com/bookmarkapp/bookmark_url/domain/Tag.java
@@ -0,0 +1,41 @@
+package com.bookmarkapp.bookmark_url.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "tags")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tag {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(nullable = false, name = "title")
+    private String title;
+
+    @Column(nullable = false, name = "description")
+    private String description;
+
+    @CreatedDate
+    @Column(name = "created_on")
+    private LocalDateTime createdOn;
+
+    @LastModifiedDate
+    @Column(name = "updated_on")
+    private LocalDateTime updatedOn;
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Url> urls;
+}
