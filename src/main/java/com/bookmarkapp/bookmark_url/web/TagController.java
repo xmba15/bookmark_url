455a09ee9701dbfdcc7f3eceb69da9749b9f6533
xmba15 <thba1590@gmail.com>
2019-07-27 13:02:10 +0900

    add subtag, tag info page

--- src/main/java/com/bookmarkapp/bookmark_url/web/TagController.java
+++ src/main/java/com/bookmarkapp/bookmark_url/web/TagController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/tags")
@@ -22,5 +24,17 @@ public class TagController {
         model.addAttribute("tags", tags);
 
         return "tags/list";
+    }
+
+    @GetMapping(path = "{id}")
+    String getTag(@PathVariable Integer id, Model model) {
+        Optional<Tag> tag = tagService.findOne(id);
+        if (tag.isPresent()) {
+            model.addAttribute("targetTag", tag.get());
+            model.addAttribute("targetSubtags", tag.get().getSubtags());
+            return "tags/info";
+        } else {
+            return "redirect:/tags";
+        }
     }
 }
