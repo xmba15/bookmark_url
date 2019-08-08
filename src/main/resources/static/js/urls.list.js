a042e11886f53f27b8537bd69aae309d12649eb4
xmba15 <thba1590@gmail.com>
2019-08-09 00:33:53 +0900

    fix js

--- src/main/resources/static/js/urls.list.js
+++ src/main/resources/static/js/urls.list.js
@@ -1,41 +1,30 @@
-// var checkboxHandlerObj = {
-//     init: function() {
-//         $('#TagSubTags
-//         input:checkbox[id^="tagIds"]').click(checkboxHandlerObj.parentClicked);
-//         $('#TagSubTags
-//         input:checkbox[id^="subTagIds"]').click(checkboxHandlerObj.childClicked)
-//     },
-
-//     parentClicked: function() {
-//         var parentCheck = this.checked;
-//         $('#TagSubTags input:checkbox[class="' + $(this).attr('id') +
-//         '-subTag"]').each(function() {
-//             this.checked = parentCheck
-//         });
-//     },
-
-//     // childClicked: function() {
-//     //     var temp = $(this).attr('class').split('-');
-//     //     var parentId = temp[0];
-//     //     $('#' + parentId)[0].checked = $('#TagSubTagss
-//     input:checkbox[class="' + $(this).attr('class') + '"]:checked').length
-//     !== 0;
-//     // }
-// };
-
 $(document).ready(function() {
   $('#TagSubTags .tag-parent').change(function() {
     var parentId = $(this).attr('id');
     var parentCheck = this.checked;
-
     var subTagRow = " .subtagrow-" + parentId;
-    var subTag = " ." + parentId + "-subTag";
+    var parentTag = parentId;
 
-    $('#TagSubTags' + subTag).each(function() { this.checked = parentCheck });
+    $('#TagSubTags input:checkbox[parent-tag="' + parentTag + '"]')
+        .each(function() {
+          this.checked = parentCheck;
+        });
 
     if (parentCheck) {
       $('#TagSubTags' + subTagRow).slideDown("fast");
     } else {
+      $('#TagSubTags' + subTagRow).slideUp("fast");
+    }
+  });
+
+  $('#TagSubTags input:checkbox[parent-tag^="tagIds"]').click(function() {
+    var parentId = $(this).attr('parent-tag');
+    var subTagRow = " .subtagrow-" + parentId;
+    var numChildChecked =
+        $('#TagSubTags input:checkbox[parent-tag="' + parentId + '"]:checked')
+            .length;
+    if (numChildChecked == 0) {
+      $('#TagSubTags #' + parentId)[0].checked = false;
       $('#TagSubTags' + subTagRow).slideUp("fast");
     }
   });
