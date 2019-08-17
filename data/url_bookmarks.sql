8fc6c0302c0b0c881facb4a64190292e722de002
xmba15 <thba1590@gmail.com>
2019-08-18 00:41:32 +0900

    update data

--- data/url_bookmarks.sql
+++ data/url_bookmarks.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:048c2e1bf80e7936dbf0549a2d03e191c8812fb6800e164813f7a4d96d1765f0
-size 16241
+oid sha256:05d5a63ceb815fb7f850ccc6d8d93b7b3e2b581c98d7a98bbeb70b6e737459ec
+size 18134
